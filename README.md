eafb781912bab105e1fec0cf57516af006a29efd
luislopezg88 <luislopezg88@gmail.com>
2023-02-15 11:55:25 -0400

    Primer commit - rest server

---
+++ README.md
@@ -0,0 +1,22 @@
+
+# Socket-Server
+
+Reconstruir los modulos de Node
+
+
+```````````
+npm install
+```````````
+
+Generar el dist
+
+```````````
+tsc -w
+```````````
+
+Levantar servidor cualquiera de estos 2 comandos
+
+```````````
+nodemon dist/
+node dist/
+```````````
---
+++ index.ts
@@ -0,0 +1,20 @@
+import Server from './classes/server';
+import { SERVER_PORT } from './global/environment';
+import router  from './routes/router';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+
+const server = new Server();
+
+server.app.use(bodyParser.urlencoded({ extended: true }));
+server.app.use(bodyParser.json());
+
+server.app.use('/', router);
+
+server.app.use( cors({ origin: true, credentials: true }));
+
+server.start(() => {
+    console.log(`El servidor esta corriendo en el puerto ${SERVER_PORT}`)
+});
+
